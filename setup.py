a23b65d5c0f28ea17a111f845c96f9f2267d3718
Rio Kierkels <riokierkels@gmail.com>
2014-09-23 15:13:34 +0200

    sytax error in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="ESLog",
     version="0.1.0",
-    install_requires=["elasticsearch"]
+    install_requires=["elasticsearch", ]
     packages=find_packages(),
 )
