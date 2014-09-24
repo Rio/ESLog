5b333058c47a8d28a615a9e545d8e5e5df013ce8
Rio Kierkels <riokierkels@gmail.com>
2014-09-24 09:39:04 +0200

    Revert "added urllib for url parsing and sending http requests to the elasticsearch service"
    This reverts commit e085a135a4bdbb3ac9bbd88795c48d572ba2f502.

--- ESLog/esloghandler.py
+++ ESLog/esloghandler.py
@@ -2,39 +2,21 @@
 
 from datetime import datetime
 import logging
-import os
-import json
-import urllib.request
-import urllib.parse
+
+from elasticsearch import Elasticsearch
 
 
 class ESLogHandler(logging.Handler):
-    def __init__(self, url, index=None, doc_type="log", level=logging.NOTSET):
-        logging.Handler.__init__(self, level=level)
+    def __init__(self, host, index, doc_type="log", level=logging.NOTSET):
+        logging.Handler.__init__(self)
+        
+        self.index = index
+        self.doc_type = doc_type
+        
+        self.es = Elasticsearch(host)
 
-        # Parse the url
-        self.url = urllib.parse.urlparse(url)
-
-        # If no scheme is given, set it to http
-        if not self.url.scheme:
-            self.url.scheme = "http"
-
-        # If a scheme is given but it is not http, raise an exception
-        elif self.url.scheme != "http":
-            raise ValueError("Only HTTP is supported.")
-
-        # If no port is given default to 9200
-        if not self.url.port:
-            self.url.port = "9200"
-
-        # If no path is given or it is only a / use thi index and doc_type to construct one.
-        if not self.url.path or self.url.path == "/":
-            # an index is mandatory for Elasticsearch, doc_type too but it defaults to log
-            if not index:
-                raise ValueError("Elasticsearch index cannot be ommitted.")
-
-            else:
-                self.url.path = os.path.join("/", index, doc_type)
+        # Set the default log level.
+        self.setLevel(level)
     # end __init__
 
     def emit(self, record):
@@ -45,10 +27,7 @@ class ESLogHandler(logging.Handler):
         message["name"] = record.name
         message["lineno"] = record.lineno
         message["message"] = record.msg
-
-        json_message = json.dumps(message)
-        json_message_bytes = json_message.encode("utf8")
-
-        urllib.request.urlopen(self.url, data=json_message_bytes)
+        
+        self.es.index(index=self.index, doc_type=self.doc_type, body=message)
     # end emit
 # end ESLogHandler
