d883a0fd09a42ff84ebb2ccf331692167370444b
Rio Kierkels <riokierkels@gmail.com>
2014-09-24 09:38:54 +0200

    Revert "trying to simplefy __init__"
    This reverts commit f2e3887bcd53b1c35af2d9dfe2363ea9e2a407f5.

--- ESLog/esloghandler.py
+++ ESLog/esloghandler.py
@@ -11,10 +11,30 @@ import urllib.parse
 class ESLogHandler(logging.Handler):
     def __init__(self, url, index=None, doc_type="log", level=logging.NOTSET):
         logging.Handler.__init__(self, level=level)
-               
+
+        # Parse the url
         self.url = urllib.parse.urlparse(url)
-        
-        print(self.url)
+
+        # If no scheme is given, set it to http
+        if not self.url.scheme:
+            self.url.scheme = "http"
+
+        # If a scheme is given but it is not http, raise an exception
+        elif self.url.scheme != "http":
+            raise ValueError("Only HTTP is supported.")
+
+        # If no port is given default to 9200
+        if not self.url.port:
+            self.url.port = "9200"
+
+        # If no path is given or it is only a / use thi index and doc_type to construct one.
+        if not self.url.path or self.url.path == "/":
+            # an index is mandatory for Elasticsearch, doc_type too but it defaults to log
+            if not index:
+                raise ValueError("Elasticsearch index cannot be ommitted.")
+
+            else:
+                self.url.path = os.path.join("/", index, doc_type)
     # end __init__
 
     def emit(self, record):
